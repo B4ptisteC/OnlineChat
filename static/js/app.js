2be0a767f3857a236b80fb19967b024344c83bba
Baptiste C <baptiste.chauviere@lyceeastier.com>
2026-05-14 16:44:35 +0200

    Add image display for valid URL input

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,8 @@
+let input = document.getElementById("messageContent");
+let value = input.value.trim();
 
+if (value.includes("https://")) {
+    let img = document.createElement("img");
+    img.src = value;
+    document.body.appendChild(img);
+}
